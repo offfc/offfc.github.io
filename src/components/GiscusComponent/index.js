0aaf2c6b31f22afe5d8d525a4141777e52206ff2
NaughtyChas <3030561972@qq.com>
2024-08-06 15:51:38 +0800

    Update giscus config

--- src/components/GiscusComponent/index.js
+++ src/components/GiscusComponent/index.js
@@ -7,21 +7,18 @@ export default function GiscusComponent() {
 
   return (
     <Giscus
-      repo="offfc/offfc.github.io"
-      repoId="R_kgDOMSC04Q  "
-      category="Announcements"
-      categoryId="DIC_kwDOMSC04c4ChbTr"
-      mapping="og:title"
-      reactionsEnabled="1"
-      emitMetadata="0"
-      inputPosition="top"
-      theme={colorMode}
-      lang="zh-CN"
-      loading="lazy"
-      term="Welcome to @giscus/react component!"
-      strict="0"
+      data-repo="offfc/offfc.github.io"
+      data-repo-id="R_kgDOMSC04Q"
+      data-category="Announcements"
+      data-category-id="DIC_kwDOMSC04c4ChbTr"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="top"
+      data-theme="preferred_color_scheme"
+      data-lang="zh-CN"
       crossorigin="anonymous"
-      async
     />
   );
 }
