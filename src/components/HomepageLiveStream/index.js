72665c457a80ebdf33a0b1195c68da29864f4f9b
NaughtyChas <3030561972@qq.com>
2024-08-08 11:13:46 +0800

    Add a `liveIndicator` to the screen
    the blinking effect of that indicator seems to be broken. Need a fix

--- src/components/HomepageLiveStream/index.js
+++ src/components/HomepageLiveStream/index.js
@@ -1,42 +1,43 @@
-import clsx from 'clsx';
+import React, { useEffect } from 'react';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
-import React from 'react';
+import './LiveIndicator.css'; // Import the CSS file
 
+// Function to resize iframe
 function resizeIframe() {
-  // get current iframe element
   const iframe = document.querySelector('.blp-activity-h5-iframe');
-
-  // aquire current page width
   const width = document.body.offsetWidth;
-
-  // acquire current page height
   const height = width * 9 / 16;
-
-  // set iframe width and height
   iframe.width = width;
   iframe.height = height;
 }
 
-// adjust iframe on load
-window.onload = resizeIframe;
-
-// adjust iframe on resize
-window.onresize = resizeIframe;
+function LiveIndicator() {
+  return (
+    <div className="live-indicator-block">
+      <span className="live-indicator">
+        <i className="fa fa-circle blink" aria-hidden="true"></i>Live
+      </span>
+    </div>
+  );
+}
 
 export default function LiveStream() {
   return (
     <section className={styles.liveStream}>
       <div className="container">
         <div className={styles.liveStreamContainer}>
-          <Heading as="h2" className={styles.liveStreamTitle}>
-            正在直播：
-          </Heading>
-            <iframe
-                src="https://www.bilibili.com/blackboard/live/live-activity-player.html?cid=21622811&sendpanel=1&danmaku=1&enableCtrlUI=1&logo=0"
-                width="1000" height="400" frameborder="no" framespacing="0" scrolling="no" allow="autoplay; encrypted-media" allowfullscreen="true"
-            ></iframe>        
-          </div>
+          <LiveIndicator />
+          <iframe
+            src="https://www.bilibili.com/blackboard/live/live-activity-player.html?cid=21622811&sendpanel=1&danmaku=1&enableCtrlUI=1&logo=0"
+            frameBorder="no"
+            frameSpacing="0"
+            scrolling="no"
+            allow="autoplay; encrypted-media"
+            allowFullScreen="true"
+            className="blp-activity-h5-iframe"
+          ></iframe>
+        </div>
       </div>
     </section>
   );
