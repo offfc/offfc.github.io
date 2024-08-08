5080e2d3e0f7dc682ba3629fbf8718e817bbb392
CloneWith <clonewith@qq.com>
2024-08-09 00:02:50 +0800

    Add FontAwesome reference

--- src/components/HomepageLiveStream/index.js
+++ src/components/HomepageLiveStream/index.js
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
-import './LiveIndicator.css'; // Import the CSS file
+import React, { useEffect } from "react";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import "./LiveIndicator.css"; // Import the CSS file
 
 // Function to resize iframe
 function resizeIframe() {
-  const iframe = document.querySelector('.blp-activity-h5-iframe');
+  const iframe = document.querySelector(".blp-activity-h5-iframe");
   const width = document.body.offsetWidth;
-  const height = width * 9 / 16;
+  const height = (width * 9) / 16;
   iframe.width = width;
   iframe.height = height;
 }
@@ -16,7 +16,15 @@ function LiveIndicator() {
   return (
     <div className="live-indicator-block">
       <span className="live-indicator">
-        <i className="fa fa-circle blink" aria-hidden="true"></i>Live
+        <link
+          rel="stylesheet"
+          href="https://cdn.staticfile.org/font-awesome/4.7.0/css/font-awesome.css"
+        ></link>
+        <i
+          className="fa fa-circle blink"
+          aria-hidden="true"
+        ></i>
+        Live
       </span>
     </div>
   );
