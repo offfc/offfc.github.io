46f9cbf2d2457a9c2998a62563d70d81d9d4b1a1
NaughtyChas <3030561972@qq.com>
2024-08-10 17:36:22 +0800

    Add sponsor banner

---
+++ src/components/SponsorBanner/SponsorBanner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './SponsorBanner.module.css';
+
+const SponsorBanner = ({imageUrl, mainText, altText}) => {
+  return (
+    <div className={styles.containerV1}>
+      <div className={styles.wrapperV1}>
+        <div className={styles.gridBlockV1}>
+          <div className={`${styles.blockElement} ${styles.green} ${styles.imageAlignSide} ${styles.imageAlignLeft} ${styles.twoByGridBlock}`}>
+            <div className={styles.blockContent}>
+              <h2>
+                <div className={styles.mainText}>
+                  <span><p>{mainText}</p></span>
+                </div>
+              </h2>
+              <div className={styles.altText}>
+                <span><p>{altText}</p></span>
+              </div>
+            </div>
+            <div className={styles.blockImage}>
+              <img src={imageUrl} alt="Sponsor" style={{ cursor: 'pointer' }} width={'20%'}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SponsorBanner;
