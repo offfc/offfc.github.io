21112c74f088ecc02c5192bf82ea2473eb298441
CloneWith <clonewith@qq.com>
2024-08-10 15:45:46 +0800

    Add FeatureButton module

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import LiveStream from '../components/HomepageLiveStream/index.js';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import HomepageTitle from '../components/HomepageTitle/HomepageTitle.js';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import LiveStream from "@site/src/components/HomepageLiveStream/index.js";
+import Heading from "@theme/Heading";
+import styles from "./index.module.css";
+import HomepageTitle from "@site/src/components/HomepageTitle/HomepageTitle.js";
+import FeatureButton from "@site/src/components/FeatureButton.js";
 
-const common = require('../../common.js');
+const common = require("../../common.js");
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   // Should we keep this button?
   // RE: sure we're gonna keep this.
   return (
     <header
-      className={clsx('hero hero--primary', styles.heroBanner)}
+      className={clsx("hero hero--primary", styles.heroBanner)}
       // style={{ backgroundImage: "url('/img/landing.gif')" }}
     >
       <div className="container">
@@ -24,35 +25,21 @@ function HomepageHeader() {
         <br />
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <a href={"/docs/handbook"} target="_self" class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
-            <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-            <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-              <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l7 7m0 0l-7 7m7-7H3"></path>
-              </svg>
-            </span>
-            <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-              <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l7 7m0 0l-7 7m7-7H3"></path>
-              </svg>
-            </span>
-            <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">查看参赛手册</span>
-          </a>
-          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-          <a href={"/docs/handbook"} target="_self" class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
-            <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
-            <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-              <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l7 7m0 0l-7 7m7-7H3"></path>
-              </svg>
-            </span>
-            <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-              <svg class="w-5 h-5 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10l7 7m0 0l-7 7m7-7H3"></path>
-              </svg>
-            </span>
-            <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">立即报名！</span>
-          </a>
+          <FeatureButton
+            link="/docs/info"
+            buttonText="了解一下"
+            spacing="1em"
+          />
+          <FeatureButton
+            link="/docs/handbook"
+            buttonText="查看参赛手册"
+            spacing="1em"
+          />
+          <FeatureButton
+            link="/docs/handbook"
+            buttonText="立即报名！"
+            spacing="1em"
+          />
         </div>
       </div>
     </header>
@@ -60,11 +47,9 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description={`${common.description}`}>
+    <Layout title={`${siteConfig.title}`} description={`${common.description}`}>
       <HomepageHeader />
       <main>
         <LiveStream />
