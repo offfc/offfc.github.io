6aa113d5b63b27676934e6c323809aaac0eb1755
NaughtyChas <3030561972@qq.com>
2024-08-10 16:00:17 +0800

    Adjusted staffCard layout

--- src/components/StaffBoard/StaffBoard.js
+++ src/components/StaffBoard/StaffBoard.js
@@ -16,10 +16,6 @@ function StaffBoard({ uid, userName, role }) {
       <br />
       <h1>{userName}</h1>
       <p class="card-title">{role}</p>
-      <a href="#" class="acard">
-        <i class="fa fa-dribbble"></i>
-      </a>
-      <br />
       <p>
         <a href={"https://osu.ppy.sh/users/" + uid} target="_blank" class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group">
           <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
