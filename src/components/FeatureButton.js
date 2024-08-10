910437d71a99711a09cf0395ff790f2fedba1339
CloneWith <clonewith@qq.com>
2024-08-10 15:48:09 +0800

    Use FeatureButton for StaffBoard page

--- src/components/FeatureButton.js
+++ src/components/FeatureButton.js
@@ -6,7 +6,7 @@ export default function FeatureButton({ link, buttonText, spacing }) {
       href={link}
       target="_self"
       class="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold shadow text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-gray-50 group"
-      style={{ marginRight: spacing }}
+      style={{ marginRight: spacing ?? 0 }}
     >
       <span class="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-indigo-600 group-hover:h-full"></span>
       <span class="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
