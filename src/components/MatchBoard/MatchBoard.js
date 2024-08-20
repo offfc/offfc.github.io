9d1f9eebecb52467b3555d7ad26007a95b5f3301
NaughtyChas <3030561972@qq.com>
2024-08-20 18:10:45 +0800

    Update MatchBoard.js

--- src/components/MatchBoard/MatchBoard.js
+++ src/components/MatchBoard/MatchBoard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './MatchBoard.module.css';
 
+
 function MatchBoard() {
   return (
     <div className={styles.matchBoardContainer}>
       <iframe 
-        src="https://challonge.com/zh_CN/8o5jwgu4/module?show_final_results=1&" 
+        /* For more module arguments, visit https://challonge.com/zh_CN/module/instructions */
+        src="https://challonge.com/zh_CN/8o5jwgu4/module?show_final_results=1&"  
         width="100%" 
         height="500" 
         frameBorder="0" 
