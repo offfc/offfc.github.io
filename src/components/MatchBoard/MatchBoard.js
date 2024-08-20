cabc36bb1cbd834b925af7ff47871d686a45c56c
NaughtyChas <3030561972@qq.com>
2024-08-20 18:09:38 +0800

    Adjust visual for matchboard

--- src/components/MatchBoard/MatchBoard.js
+++ src/components/MatchBoard/MatchBoard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import styles from './MatchBoard.module.css';
 
-function MatchBoard (){
-  return(
-    <iframe 
-    src="https://challonge.com/zh_CN/8o5jwgu4/module" 
-    width="100%" 
-    height="500" 
-    frameborder="0" 
-    scrolling="auto" 
-    allowtransparency="true">
-    </iframe>
-  )
+function MatchBoard() {
+  return (
+    <div className={styles.matchBoardContainer}>
+      <iframe 
+        src="https://challonge.com/zh_CN/8o5jwgu4/module?show_final_results=1&" 
+        width="100%" 
+        height="500" 
+        frameBorder="0" 
+        scrolling="auto" 
+        allowTransparency="true">
+      </iframe>
+    </div>
+  );
 }
-
 
 export default MatchBoard;
