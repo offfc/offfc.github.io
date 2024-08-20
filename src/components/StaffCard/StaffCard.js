a69837f63adb726103423c33db08fc3425df2dad
CloneWith <clonewith@qq.com>
2024-08-21 00:42:16 +0800

    Lint StaffCard style

--- src/components/StaffCard/StaffCard.js
+++ src/components/StaffCard/StaffCard.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./StaffCard-module.css";
 import FeatureButton from "../FeatureButton/FeatureButton.js";
 
+// TODO: Bottom margin won't work, why?
 function StaffCard({ uid, userName, role }) {
   return (
       <div className="card">
       <img
         src={"https://a.ppy.sh/" + uid}
-        alt=""
+        alt="User Avatar"
         style={{ width: 100 + "%" }}
       />
-      <h1>{userName}</h1>
-      <p className="card-title">{role}</p>
-      <p style={{ marginTop: "10px" }}>
+      <p className="card-title">{userName}</p>
+      <p className="card-subtitle">{role}</p>
+      <p style={{ marginTop: "1rem", marginBottom: "1rem" }}>
         <FeatureButton link={ "https://osu.ppy.sh/users" + uid} buttonText="在 osu! 查看"/>
       </p>
       <br />
