fb743b1835b8a3d2463ba7bfdc827260704678e0
NaughtyChas <3030561972@qq.com>
2024-08-25 00:02:58 +0800

    Fix animated background covers text
    might not be a complete fix as the `hero__subtitle` seems to have no effect

--- src/components/HomepageTitle/HomepageTitle.js
+++ src/components/HomepageTitle/HomepageTitle.js
@@ -8,23 +8,35 @@ export default function HomepageTitle() {
                 style={{
                     WebkitBackgroundClip: "text",
                     backgroundClip: "text",
-                    backgroundImage: "linear-gradient(to left, #3e9efe, #378bef, #3378df, #3065cf)"
+                    backgroundImage: "linear-gradient(to left, #3e9efe, #378bef, #3378df, #3065cf)",
+                    zIndex: 3, // ← This basically ensures text is above the circles
+                    position: "relative"
                 }} 
                 className="font-extrabold text-transparent bg-clip-text text-7xl md:text-8xl"
             >
                 osu! FumoFumo Cup
             </span>
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+            <ul 
+                className="circles"
+                style={{
+                    zIndex: 1,
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
             </ul>
         </div>
     );
