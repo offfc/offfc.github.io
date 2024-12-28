c1a341bfeeac05b89866824fa2c64152b72a3f17
CloneWith <clonewith@qq.com>
2024-12-28 11:55:43 +0800

    Use FontAwesome package for icon reference
    The icon set in the stylesheet is too old and cannot work properly now.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,4 +1,3 @@
-import React from "react";
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents";
 import Highlight from "@site/src/components/Highlight";
@@ -6,15 +5,17 @@ import FntColor from "@site/src/components/FntColor";
 import StaffCard from "@site/src/components/StaffCard/StaffCard";
 import FeatureButton from "@site/src/components/FeatureButton/FeatureButton";
 import Button from "@mui/material/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default {
-  // Re-use the default mapping
-  ...MDXComponents,
-  // Map the "<Highlight>" tag to our Highlight component
-  // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
-  Highlight,
-  FntColor,
-  StaffCard,
-  FeatureButton,
-  Button
+    // Re-use the default mapping
+    ...MDXComponents,
+    // Map the "<Highlight>" tag to our Highlight component
+    // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
+    Highlight,
+    FntColor,
+    StaffCard,
+    FeatureButton,
+    Button,
+    FontAwesomeIcon
 };
