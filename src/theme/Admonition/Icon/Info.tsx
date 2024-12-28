02594edc35b1c6cc73e5ff5adaa87fff5672dd69
CloneWith <clonewith@qq.com>
2024-12-28 12:14:58 +0800

    Customize icons for Admonitions
    These icons are from MUI and FontAwesome.

---
+++ src/theme/Admonition/Icon/Info.tsx
@@ -0,0 +1,19 @@
+﻿/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React, {type ReactNode} from 'react';
+import type {Props} from '@theme/Admonition/Icon/Info';
+
+export default function AdmonitionIconInfo(props: Props): ReactNode {
+    return (
+        <svg viewBox="0 0 24 24" {...props}>
+            <path
+                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m1 15h-2v-6h2zm0-8h-2V7h2z"
+            />
+        </svg>
+    );
+}
