a237f8f75c9c1892991efd4bec2c9efe1d367d92
NaughtyChas <3030561972@qq.com>
2025-06-15 14:42:25 +0800

    Re-design the hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,26 +13,62 @@ import HomepageFeatureDisplay from "../components/HomepageFeatureDisplay";
 const common = require("../../common.js");
 
 function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header
-      className={clsx("hero hero--primary", styles.heroBanner)}
-    >
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <HomepageTitle/>
-        <br/>
-        <div className={styles.buttons}>
-          <FeatureButton
-            link="/docs/info"
-            buttonText="了解一下"
-          />
-          <FeatureButton
-            link="/docs/handbook"
-            buttonText="查看参赛手册"
-          />
-          <FeatureButton
-            link="https://docs.qq.com/form/page/DV1hXY1RMU1FOeGlM"
-            buttonText="立即报名！"
-          />
+        <div className={styles.heroContent}>
+          <div className={styles.heroLeft}>
+            <div className={styles.titleSection}>
+              <HomepageTitle />
+              <div className={styles.titleUnderline}></div>
+            </div>
+
+            <div className={styles.descriptionSection}>
+              <h2 className={styles.tagline}>h2写这里</h2>
+              <p className={styles.heroDescription}>
+                内容写这里
+              </p>
+
+              <div className={styles.statsGrid}>
+                <div className={styles.statItem}>
+                  <div className={styles.statNumber}>4v4</div>
+                  <div className={styles.statLabel}>对战规模</div>
+                </div>
+                <div className={styles.statItem}>
+                  <div className={styles.statNumber}>8</div>
+                  <div className={styles.statLabel}>参赛队伍</div>
+                </div>
+                <div className={styles.statItem}>
+                  <div className={styles.statNumber}>4* - 7*</div>
+                  <div className={styles.statLabel}>图池难度</div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.heroRight}>
+            <div className={styles.actionPanel}>
+              <div className={styles.panelHeader}>
+                <h3 className={styles.panelTitle}>开始你的旅程</h3>
+                <p className={styles.panelSubtitle}>选择你的下一步行动</p>
+              </div>
+              <div className={styles.buttonGroup}>
+                <FeatureButton
+                  link="/docs/info"
+                  buttonText="了解一下"
+                />
+                <FeatureButton
+                  link="/docs/handbook"
+                  buttonText="查看参赛手册"
+                />
+                <FeatureButton
+                  link="https://docs.qq.com/form/page/DV1hXY1RMU1FOeGlM"
+                  buttonText="立即报名！"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </header>
@@ -43,7 +79,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={`${siteConfig.title}`} description={`${common.description}`}>
-      <HomepageHeader/>
+      <HomepageHeader />
       <main>
         <HomepageFeatureDisplay/>
         <HomepageNotification type={"info"} message={
