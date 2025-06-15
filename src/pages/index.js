88df20b99b734b07918f487bf47f5267e81748c0
NaughtyChas <3030561972@qq.com>
2025-06-15 18:52:42 +0800

    Add a grid effect for hero background

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,55 @@ import LiveStream from "../components/HomepageLiveStream";
 import HomepageFeatureDisplay from "../components/HomepageFeatureDisplay";
 
 const common = require("../../common.js");
+import { useEffect } from 'react';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
+  useEffect(() => {
+    const heroBanner = document.querySelector(`.${styles.heroBanner}`);
+
+    const handleMouseMove = (e) => {
+      if (heroBanner) {
+        const rect = heroBanner.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        heroBanner.style.setProperty('--mouse-x', `${x}px`);
+        heroBanner.style.setProperty('--mouse-y', `${y}px`);
+      }
+    };
+
+    const handleMouseLeave = () => {
+      if (heroBanner) {
+        heroBanner.style.setProperty('--mouse-x', '-300px');
+        heroBanner.style.setProperty('--mouse-y', '-300px');
+      }
+    };
+
+    const handleMouseEnter = () => {
+      if (heroBanner) {
+        const rect = heroBanner.getBoundingClientRect();
+        heroBanner.style.setProperty('--mouse-x', `${rect.width / 2}px`);
+        heroBanner.style.setProperty('--mouse-y', `${rect.height / 2}px`);
+      }
+    };
+
+    if (heroBanner) {
+      heroBanner.addEventListener('mousemove', handleMouseMove);
+      heroBanner.addEventListener('mouseleave', handleMouseLeave);
+      heroBanner.addEventListener('mouseenter', handleMouseEnter);
+    }
+
+    return () => {
+      if (heroBanner) {
+        heroBanner.removeEventListener('mousemove', handleMouseMove);
+        heroBanner.removeEventListener('mouseleave', handleMouseLeave);
+        heroBanner.removeEventListener('mouseenter', handleMouseEnter);
+      }
+    };
+  }, []);
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
